d510a8b3077e4f67b6acf37e89d0ac03b9d2e594
jmcacioppo <jmcacioppo@bellsouth.net>
2017-01-21 18:30:05 -0500

    adding pics

--- solution.js
+++ solution.js
@@ -1,4 +1,10 @@
 $(".ask").click(function() {
 	$(".welcome").hide();
 
+	if($(".question").val() == 'alpha') {
+		$(".play").append('<p>This is your play!</p>');
+		$(".play").append('<img src = "34SHT_Pinch.PNG"/>');
+	}
+
 });
+
