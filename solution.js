2b27df7de4a8e25f603c24ebb4dbcf4f437e59bd
jmcacioppo <jmcacioppo@bellsouth.net>
2017-01-21 16:19:16 -0500

    add solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,10 @@
-function 
+var x = document.getElementsByClassName("ask");
+
+function ask() {
+	x.style.visibility = "hidden";
+	var btn = document.createElement("askagain");
+}
+
+function askAgain() {
+	x.style.visibility = "visible";
+}
