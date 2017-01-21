4dfdba628b99be58b750a421533cc7aaa74e7b27
loloduca <hbolivar@ymail.com>
2017-01-21 17:48:11 -0500

    jQuery

--- solution.js
+++ solution.js
@@ -1,11 +1,3 @@
-function ask() {
-	var x = document.getElementsByClassName("ask");
-	x.style.visibility = "hidden";
-	var y = document.getElementById("question").value;
-	alert(y);
-	// var btn = document.createElement("askagain");
-}
-
-function askAgain() {
-	x.style.visibility = "visible";
-}
+$(".ask").click(function() {
+	$(".welcome").hide();
+});
