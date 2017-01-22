0fc2813848afb51414e65dadc0fb132911741bc3
veryGoldenPants <jerodriguez67@gmail.com>
2017-01-21 19:51:16 -0500

    SHits good

--- solution.js
+++ solution.js
@@ -1,11 +1,26 @@
+var check = true;
+$(document).ready(function(){
+	$(".reset").hide();
+})
+
 $(".ask").click(function() {
-	if($(".question").val() == true) {
+	if($(".question").val() == false && check == true) {
+		$(".fail").append('<p>You need to ask Madden.</p>');
+		check = false;
+	}else if($(".question").val() != false && check == false){
 		$(".welcome").hide();
+		$(".fail").remove();
 		$(".play").append('<p>This is your play!</p>');
 		if($(".question").val() == "alpha") {
 			$(".play").append('<img src = "34SHT_Pinch.PNG"/>');
 		}
+		check = false;
+		$(".reset").show();
+		$(".question").hide();
+		$(".ask").hide();
 	}
+})
 
+$(".reset").click(function(){
+	location.reload();
 });
-
