7691e7f6ae5f2719c17f3d9fa0904e91d8014a17
jmcacioppo <jmcacioppo@bellsouth.net>
2017-01-22 12:25:33 -0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -25,52 +25,52 @@ $(".ask").click(function() {
 			 $(".question").val().search("fight") || $(".question").val().search("choke") || 
 			 $(".question").val().search("attack") || $(".question").val().search("gun") || 
 			 $(".question").val().search("knife") != -1) {
-			if(x > 0 && x < 5) {
+			if(x >= 0 && x < 10) {
 				$(".play").append('<img src = "ILeft37Pwr.PNG"/>');
 			}
-			else if(x > 10 && x < 15) {
+			else if(x >= 10 && x < 15) {
 				$(".play").append('<img src = "IRight24FakeGoPass.PNG"/>');
 			}
-			else if(x > 15 && x < 20) {
+			else if(x >= 15 && x < 20) {
 				$(".play").append('<img src = "IRight33Dive.PNG"/>');
 			}
-			else if(x > 20 && x < 25) {
+			else if(x >= 20 && x < 25) {
 				$(".play").append('<img src = "IRightSplit31Fake24Cross.PNG"/>');
 			}
-			else if(x > 25 && x < 30) {
+			else if(x >= 25 && x < 30) {
 				$(".play").append('<img src = "Shotgun860_861.PNG"/>');
 			}
-			else if(x > 30 && x < 35) {
+			else if(x >= 30 && x < 35) {
 				$(".play").append('<img src = "ShotgunBlue897GunPass.PNG"/>');
 			}
-			else if(x > 35 && x < 40) {
+			else if(x >= 35 && x < 40) {
 				$(".play").append('<img src = "ShotgunRed896Run.PNG"/>');
 			}
-			else if(x > 40 && x < 45) {
+			else if(x >= 40 && x < 45) {
 				$(".play").append('<img src = "ShotgunRed898GunPass.PNG"/>');
 			}
-			else if(x > 45 && x < 50) {
+			else if(x >= 45 && x < 50) {
 				$(".play").append('<img src = "TFormLeftFlankerPass.PNG"/>');
 			}
-			else if(x > 50 && x < 55) {
+			else if(x >= 50 && x < 55) {
 				$(".play").append('<img src = "TFormLeftPitchTEReverse.PNG"/>');
 			}
-			else if(x > 55 && x < 60) {
+			else if(x >= 55 && x < 60) {
 				$(".play").append('<img src = "TFormRightFakeBootPass.PNG"/>');
 			}
-			else if(x > 60 && x < 65) {
+			else if(x >= 60 && x < 65) {
 				$(".play").append('<img src = "TFormRightFakePitch.PNG"/>');
 			}
-			else if(x > 65 && x < 70) {
+			else if(x >= 65 && x < 70) {
 				$(".play").append('<img src = "Wing121Jet.PNG"/>');
 			}
-			else if(x > 70 && x < 75) {
+			else if(x >= 70 && x < 75) {
 				$(".play").append('<img src = "Wing121Toss.PNG"/>');
 			}
-			else if(x > 75 && x < 80) {
+			else if(x >= 75 && x < 80) {
 				$(".play").append('<img src = "Wing182Counter.PNG"/>');
 			}
-			else if(x > 80 && x < 85) {
+			else if(x >= 80 && x <= 100) {
 				$(".play").append('<img src = "WingWedge144CTXXDive.PNG"/>');
 			}
 		}
@@ -87,16 +87,16 @@ $(".ask").click(function() {
 			$(".question").val().search("boyfriend") || $(".question").val().search("girlfriend") || 
 			$(".question").val().search("fiancee") || $(".question").val().search("car") || 
 			$(".question").val().search("house") != -1) {
-			if(x > 0 && x < 25) {
+			if(x >= 0 && x < 25) {
 				$(".play").append('<img src = "34OddMarlinExchange.PNG"/>');
 			}
-			else if(x > 25 && x < 50) {
+			else if(x >= 25 && x < 50) {
 				$(".play").append('<img src = "34QueensDeep.PNG"/>');
 			}
-			else if(x > 50 && x < 75) {
+			else if(x >= 50 && x < 75) {
 				$(".play").append('<img src = "34SHT_Pinch.PNG"/>');
 			}
-			else if(x > 75 && x < 100) {
+			else if(x >= 75 && x <= 100) {
 				$(".play").append('<img src = "34UnderPirate.PNG"/>');
 			}
 		} else $(".play").append('<img src = "HailMary.png"/>');
