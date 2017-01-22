ee80b1760a902a54c54f0d4a3ded17fbb20387f6
jmcacioppo <jmcacioppo@bellsouth.net>
2017-01-21 19:13:55 -0500

    js

--- solution.js
+++ solution.js
@@ -1,9 +1,10 @@
 $(".ask").click(function() {
-	$(".welcome").hide();
-
-	if($(".question").val() == 'alpha') {
+	if($(".question").val() == true) {
+		$(".welcome").hide();
 		$(".play").append('<p>This is your play!</p>');
-		$(".play").append('<img src = "34SHT_Pinch.PNG"/>');
+		if($(".question").val() == "alpha") {
+			$(".play").append('<img src = "34SHT_Pinch.PNG"/>');
+		}
 	}
 
 });
